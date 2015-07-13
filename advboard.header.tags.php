c467da14abf2f2c722c3e56c6bb0e913422ee395
Alexey Kalnov <kalnovalexey@yandex.ru>
2015-07-13 13:14:07 +0300

    fix

---
+++ advboard.header.tags.php
@@ -0,0 +1,23 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Hooks=header.tags
+[END_COT_EXT]
+==================== */
+/**
+ * Ads board module for Cotonti Siena
+ *
+ * @package Advboard
+ * @author Kalnov Alexey    <kalnovalexey@yandex.ru>
+ * @copyright Portal30 Studio http://portal30.ru
+ */
+defined('COT_CODE') or die('Wrong URL.');
+
+if (!COT_AJAX && defined('COT_ADMIN') && $cfg['admintheme'] == 'cpanel'){
+
+    $admin_MenuUser['advboard'] = array(
+        'title' => cot::$L['advboard_my_ads'],
+        'url' => cot_url('advboard', array('m'=>'user')),
+        'icon_class' => 'fa fa-file-text',
+    );
+}
